4fd85455193ac621a956818f372fcb06edd5c18c
bawdiest <com@mikmak.cc>
2017-04-22 14:35:56 +0200

    Enable Response Messages

--- airigareMQTTclient.py
+++ airigareMQTTclient.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import pexpect, sys, json, select, time
+import pexpect, sys, json, select, time, urlparse
 import paho.mqtt.client as paho
 
 # Constants
 bluetooth_adr = "CB:FE:19:98:A0:EF"
 sysid = "79cf6c22-dcc6-11e5-8e77-00113217113f"
-
+url_str = 'mqtt://mikmak.cc:1883'
 
 # LoPoSwitch
 def floatfromhex(h):
@@ -17,28 +17,36 @@ def floatfromhex(h):
     return t
 
 class LoPoSwitch:
+	pexpectCommandTimeOut = 3
 
 	def __init__( self, bluetooth_adr ):
+		self.bluetooth_adr = bluetooth_adr
+		self.connect()
+
+
+	def connect(self):
 		r = 0
-		self.con = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive -t random --listen')
+		self.con = pexpect.spawn('gatttool -b ' + self.bluetooth_adr + ' --interactive -t random --listen')
 		#self.con.logfile = open(self.file, "a") #Dissable for Python3
-		self.con.expect('\[LE\]>', timeout=100)
+		self.con.expect('\[LE\]>', timeout=self.pexpectCommandTimeOut)
 		#print "Preparing to connect."
 		self.con.sendline('connect')
 		# test for success of connect
-		r = self.con.expect(['Connection successful.*\[LE\]>', pexpect.TIMEOUT])
-		print(r)
-		# Earlier versions of gatttool returned a different message.  Use this pattern -
-		#self.con.expect('\[CON\].*>')
+		r = self.con.expect(['Connection successful.*\[LE\]>', pexpect.TIMEOUT], timeout = self.pexpectCommandTimeOut)
+		print("Connection successful.*\[LE\]>'", r)
+		return r
+
+
 		self.con.sendline('char-write-req 0x000e 0100')
-		self.con.expect('Characteristic value was written successfully')
-		print(r)
+		r = self.con.expect(['Characteristic value was written successfully', pexpect.TIMEOUT], timeout = self.pexpectCommandTimeOut)
+		print("Characteristic value was written successfully", r)
+		return r
 
 	def turnOn(self):
 		r = 0
 		cmd = 'char-write-cmd 0x000b 5231' #Write 'R1' to second attribute
 		self.con.sendline( cmd )
-		r = self.con.expect(['Notification handle = 0x000d value: ', 'Command Failed: Disconnected'])
+		r = self.con.expect(['Notification handle = 0x000d value: ', 'Command Failed: Disconnected', pexpect.TIMEOUT], timeout = self.pexpectCommandTimeOut)
 		print(r)
 		return r
 
@@ -47,20 +55,16 @@ class LoPoSwitch:
 		cmd = 'char-write-cmd 0x000b 5230' #Write 'R0' to second attribute
 		#print cmd
 		self.con.sendline( cmd )
-		r = self.con.expect(['Notification handle = 0x000d value: ', 'Command Failed: Disconnected'])
+		r = self.con.expect(['Notification handle = 0x000d value: ', 'Command Failed: Disconnected', pexpect.TIMEOUT], timeout = self.pexpectCommandTimeOut)
 		print(r)
 		return r
 
 
 # Connect to BLE LoPoSwitch
-try:
-	AIrigarePump = LoPoSwitch(bluetooth_adr) 
-except Exception:
-	print("Not Connected to BLE")
-	pass
+AIrigarePump = LoPoSwitch(bluetooth_adr)
 
 # Define Variable to be able to count Pumping Time (Duration)
-sT = time()
+sT = time.time()
 
 
 # Define event callbacks
@@ -71,31 +75,30 @@ def on_message(mosq, obj, msg):
 	if msg.payload == "1":
 		print("Turning on...")
 		mqttc.publish(sysid + "/loposwitch/RX", "Turning on...")
-		try:
-			global AIrigarePump
-			AIrigarePump.turnOn() 
-		except Exception:
-			print("Not Connected to BLE")
-			mqttc.publish(sysid + "/loposwitch/RX", "Not Connected")
-			pass
 
-		sT = time()
+		r = AIrigarePump.turnOn()
+		if r == 0:
+			mqttc.publish(sysid + "/loposwitch/RX", "OK")
+		#elif r == 1:
+		else:
+			mqttc.publish(sysid + "/loposwitch/RX", "Not OK...")
 	
-	if msg.payload == "0":
+	if msg.payload == "0": # Turn Off
 		print("Turning off...")
-		mqttc.publish(sysid + "/loposwitch/RX", "Turning off...")
-		try:
-			global AIrigarePump
-			AIrigarePump.turnOff() 
-		except Exception:
-			mqttc.publish(sysid + "/loposwitch/RX", "Not Connected")
-			print("Not Connected to BLE")
-			pass
-		
-		global sT	
-		wT = time() - sT
-		print("I was watering " + str(round(wT,0)) + " seconds")
-		mqttc.publish(sysid + "/loposwitch/RX", "I was watering " + str(round(wT,0)) + " seconds")
+		mqttc.publish(sysid + "/loposwitch/RX", "Turning off...")		
+		AIrigarePump.turnOff()
+
+		r = AIrigarePump.turnOff()
+		if r == 0:
+			mqttc.publish(sysid + "/loposwitch/RX", "OK")
+			global sT	
+			waT = time.time() - sT
+			print("I was watering " + str(round(wT,0)) + " seconds")
+			mqttc.publish(sysid + "/loposwitch/RX", "I was watering " + str(round(wT,0)) + " seconds")
+		elif r == 1:
+			mqttc.publish(sysid + "/loposwitch/RX", "Not OK...")
+		elif r == 2:
+			mqttc.publish(sysid + "/loposwitch/RX", "Some Other Error")
     	
 	print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
@@ -118,8 +121,6 @@ mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
 #mqttc.on_log = on_log
 
-# Parse CLOUDMQTT_URL (or fallback to localhost)
-url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://mikmak.cc:1883')
 url = urlparse.urlparse(url_str)
 
 # Connect
